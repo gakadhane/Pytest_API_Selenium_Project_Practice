880434278bdf464e80ae29af68c69a50e3b4b4c2
Gaurav Kadhane <gakadhane@gmail.com>
2024-11-22 07:24:38 +0530

    Initial commit

---
+++ Selenium Practice/Locators.py
@@ -0,0 +1,14 @@
+# Locators in selenium
+#
+# id
+# name =
+# classname
+# xpath
+# Absolute xpath == html/body/table/td == denoting = immediate chile
+# Relative xpath == //td == denoting = any child
+# syntax == //tagname[@attribute='value']
+# linktext
+# partiallinktext
+#
+#
+#
